a4c4e4e8bf2f7dfe481bb62bd5a06d64be001d98
unknown <luisrodriguzzr@gmail.com>
2023-01-28 17:47:00 -0500

    Finalizado

--- api/src/app.js
+++ api/src/app.js
@@ -20,33 +20,12 @@ server.use(morgan('dev'));
 
 
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
-
-// esto es para el deploy
-
-/* const { ALLOW_ALL} = require('./DB_variables.js');
-require('dotenv').config();
-
-server.use((req, res, next) => {
-  const allowedOrigins = [ process.env.DEPLOY_URL, 'https://pi-food-production-16ac.up.railway.app/', 'http://localhost:3000'];
-  const origin = req.headers.origin;
-  
-  if (ALLOW_ALL === 1) {
-    res.setHeader('Access-Control-Allow-Origin', "*");
-  }else if (allowedOrigins.includes(origin)) {
-       res.setHeader('Access-Control-Allow-Origin', origin);
-  }
-
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-}); */
 
 // esto sigue como antes:
 
